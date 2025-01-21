d5fb2b8314057aaee085d24ce9edd0f827dc8dd6
Jeison <jeisonortiz1516@gmail.com>
2025-01-21 11:54:06 -0500

    update link buttons and margins

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,28 +32,31 @@ export default function RootLayout({
         <div className="relative flex flex-col items-center justify-center min-h-screen bg-cover bg-center background-image">
           {/* Dark overlay */}
           <div className="absolute inset-0 bg-black bg-opacity-50"></div>
-          <div className="relative z-10 grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+          <div className="z-10 grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
             <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-              <div className="bg-white p-8 rounded-lg shadow-md w-96">
-                <h1 className="text-2xl font-bold mb-6 text-center text-gray-950">
+              <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-lg">
+                <h1 className="text-3xl font-bold mb-8 text-center text-gray-800">
                 🎮 Mini Games Collection
                 </h1>
-                <ul>
+                <ul className="flex gap-4 flex-wrap justify-center">
                   <li>
-                    <Link href="/pages/coinflip">Coin Flip Game</Link>
+                    <Link className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition" href="/pages/coinflip">Coin Flip Game</Link>
                   </li>
                   <li>
-                    <Link href="/pages/numberguessing">
+                    <Link className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition" href="/pages/numberguessing">
                       Number Guessing Game
                     </Link>
                   </li>
                   <li>
-                    <Link href="/pages/rockpaperscissors">
+                    <Link className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition" href="/pages/rockpaperscissors">
                       Rock Paper Scissors Game
                     </Link>
                   </li>
                 </ul>
+                <div className="mt-8">
+
                 {children}
+                </div>
               </div>
             </main>
           </div>
