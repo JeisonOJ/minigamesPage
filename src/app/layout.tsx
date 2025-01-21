9e6a34953eeaa5f4e7f9734c56169ce56a0a13b0
Jeison <jeisonortiz1516@gmail.com>
2025-01-21 12:13:17 -0500

    add background position

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,9 +27,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased backgroundImage`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased backgroundImage bg-cover backgroundPosition`}
       >
-        <div className="relative flex flex-col items-center justify-center min-h-screen bg-cover bg-center background-image">
+        <div className="relative flex flex-col items-center justify-center min-h-screen bg-cover bg-center">
           {/* Dark overlay */}
           <div className="absolute inset-0 bg-black bg-opacity-50"></div>
           <div className="z-10 grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
