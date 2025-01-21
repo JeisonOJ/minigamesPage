0aa179f6b49e99b83edd5a8a21530fa60e632b33
Jeison <jeisonortiz1516@gmail.com>
2025-01-21 11:29:00 -0500

    add image and styles to the background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,35 +27,38 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased backgroundImage`}
       >
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-          <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-            <div className="bg-white p-8 rounded-lg shadow-md w-96">
-              <h1 className="text-2xl font-bold mb-6 text-center text-gray-950">
-                Mini Games Collection
-              </h1>
-              <ul>
-                <li>
-                  <Link href="/coinflip">Coin Flip Game</Link>
-                </li>
-                <li>
-                  <Link href="/components/coinflip">Coin Flip Game</Link>
-                </li>
-                <li>
-                  <Link href="/components/numberguessing">Number Guessing Game</Link>
-                </li>
-                <li>
-                  <Link href="/components/rockpaperscissors">
-                    Rock Paper Scissors Game
-                  </Link>
-                </li>
-              </ul>
-              {children}
-            </div>
-          </main>
+        <div className="relative flex flex-col items-center justify-center min-h-screen bg-cover bg-center background-image">
+          {/* Dark overlay */}
+          <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+          <div className="relative z-10 grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+            <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+              <div className="bg-white p-8 rounded-lg shadow-md w-96">
+                <h1 className="text-2xl font-bold mb-6 text-center text-gray-950">
+                🎮 Mini Games Collection
+                </h1>
+                <ul>
+                  <li>
+                    <Link href="/pages/coinflip">Coin Flip Game</Link>
+                  </li>
+                  <li>
+                    <Link href="/pages/numberguessing">
+                      Number Guessing Game
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/pages/rockpaperscissors">
+                      Rock Paper Scissors Game
+                    </Link>
+                  </li>
+                </ul>
+                {children}
+              </div>
+            </main>
+          </div>
+          <Footer></Footer>
         </div>
-        <Footer></Footer>
       </body>
     </html>
   );
