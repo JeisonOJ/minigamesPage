878fa26e618046993b60cd7c5169c339f60b25e8
Jeison <jeisonortiz1516@gmail.com>
2025-01-21 11:53:36 -0500

    add a text shadow white and fix tailwind styles

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,20 +1,13 @@
-import React from 'react';
+import React from "react";
 
 const Footer: React.FC = () => {
-    return (
-        <footer className={`${footerStyle} z-10 font-bold`}>
-            <p className='shadowWhite'>&copy; {new Date().getFullYear()} Jeison Ortiz. All rights reserved.</p>
-        </footer>
-    );
-};
-
-const footerStyle: React.CSSProperties = {
-    position: 'fixed',
-    bottom: 0,
-    width: '100%',
-    color: '#222',
-    textAlign: 'center',
-    padding: '10px 0'
+  return (
+    <footer className="fixed bottom-0 w-full text-center p-2 z-10 font-bold">
+      <p className="shadowWhite">
+        &copy; {new Date().getFullYear()} Jeison Ortiz. All rights reserved.
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
