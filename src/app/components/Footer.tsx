cef30250321daac1bfc3f0f4cb29cb222c53747d
Jeison <jeisonortiz1516@gmail.com>
2025-01-21 13:46:06 -0500

    remove fixed footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="fixed bottom-0 w-full text-center p-2 z-10 font-bold">
+    <footer className="absolute bottom-0 w-full text-center p-2 z-10 font-bold">
       <p className="shadowWhite">
         &copy; {new Date().getFullYear()} Jeison Ortiz. All rights reserved.
       </p>
